117275fcdd9638a78130ba820ef1896e36f7e560
Giovanni <giovanni.dallava@gadz.org>
2018-05-18 10:03:56 +0200

    wip

--- setup.py
+++ setup.py
@@ -2,11 +2,25 @@
 
 from distutils.core import setup
 
-setup(name='ProxyClient',
+setup(name='ftp-proxy-client',
       version='1.0',
-      description='Client for Proxy',
+      description='Client for FtpProxy',
       author='Giovanni Dallava',
       author_email='giovanni.dallava@uptilab.com',
       url='https://github.com/uptilab2/ftp-proxy-client',
-      packages=['requests']
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords='ftp proxy client',
+      install_requires=['requests']
+      extras_require={  # Optional
+        'dev': ['pytest', 'flake8'],
+      },
+      project_urls={
+        'Source': 'https://github.com/uptilab2/ftp_proxy',
+      },
      )
